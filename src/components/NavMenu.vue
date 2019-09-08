f6819e55fdb34de22c43cdb77977fbb036f617e4
Gary <1069929341@qq.com>
2019-09-08 19:00:36 +0800

    对导航栏进行了布局

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,59 +1,66 @@
 <template>
-<div>
-  <div id="head"><span id="aside-logo"></span> 
- <span>匠心项目管理</span>   </div>
-<div id="menu">
-<el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
- <el-menu-item index="1" >我的任务</el-menu-item>
- <el-submenu   index="2">
-    <template slot="title">项目管理</template>
-    <el-menu-item index="2-1">选项1</el-menu-item>
-    <el-menu-item index="2-2">选项2</el-menu-item>
-    <el-menu-item index="2-3">选项3</el-menu-item>
-    <el-submenu index="2-4">
-      <template slot="title">选项4</template>
-      <el-menu-item index="2-4-1">选项1</el-menu-item>
-      <el-menu-item index="2-4-2">选项2</el-menu-item>
-      <el-menu-item index="2-4-3">选项3</el-menu-item>
-    </el-submenu></el-submenu>
-  <el-menu-item index="3" >进度报表</el-menu-item> 
-  <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">知识库</a></el-menu-item>
-</el-menu>
-</div>
-</div>
-
+  <el-menu :default-active="activeIndex" class="nav_menu" mode="horizontal">
+    <li id="head">
+      <span id="head_logo"></span>
+    </li>
+    <li id="projectname">匠心项目管理系统</li>
+    <el-menu-item index="1">我的任务</el-menu-item>
+    <el-menu-item index="2">项目管理</el-menu-item>
+    <el-menu-item index="3">进度报表</el-menu-item>
+    <el-menu-item index="4">知识库</el-menu-item>
+    <li id="info">
+    <el-avatar icon="el-icon-user-solid"></el-avatar>
+    </li>
+  </el-menu>
 </template>
 
 <script>
 export default {
-  name: 'NavMenu'
-}
+  name: "NavMenu"
+};
 </script>
-
-
 <style scoped>
-#aside-logo{
-    width: 42px;
-    height: 42px;
-    margin-right: 15px;
-    line-height: 60px;
-    display: inline-block;
-    border-radius: 21px;
-    background: url('../assets/logo.png') no-repeat;
-    background-size: 42px 42px;
+#navigator {
+  height: 100px;
+  line-height: 30px;
+  background-color: chartreuse;
+  color: #999;
+  display: block;
+  list-style-type: disc;
+  margin-block-start: 1em;
+  margin-block-end: 1em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  padding-inline-start: 40px;
+}
+#info {
+  margin-top: 10px;
+  float: right;
 }
 
-#head,#menu{
-    background-color:#459898;
-    position:absolute;
-} 
-#head{
-    top:20px;
-    left:20px;
+#head {
+  display: block;
+
+  float: left;
 }
-#menu{
-    top:20px;
-    left:260px;
-} 
+#head_logo {
+  margin-top: 10px;
+  width: 42px;
+  height: 42px;
+  margin-right: 15px;
+  line-height: 60px;
+  display: inline-block;
+  border-radius: 21px;
+  background: url("../assets/logo.png") no-repeat;
+  background-size: 42px 42px;
+}
+
+#projectname {
+  margin-top: 13px;
+  display: block;
+  float: left;
+  margin-right: 400px;
+  font-size: 20px;
+}
 </style>
 

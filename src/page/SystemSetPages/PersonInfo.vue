dc6b808d06e48dd9e77c8940edca3b2b576e7fa9
Jay <1547059772@qq.com>
2019-09-26 21:34:34 +0800

    鄢敏杰—2019/9/26/21：34

--- src/page/SystemSetPages/PersonInfo.vue
+++ src/page/SystemSetPages/PersonInfo.vue
@@ -1,77 +1,129 @@
   <template>
   <el-container>
-    <el-table
-      :data="tableData"
-     >
-      <el-table-column
-        prop="date"
-        label="日期"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="name"
-        label="姓名"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="address"
-        label="地址">
-      </el-table-column>
-    </el-table>
-    </el-container>
-  </template>
+    <el-header style="height：70px;">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item :to="{ path: 'G:\资料\做项目\VueProjects\ZJ_PM\src\page\MyTask.vue' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>
+          <a href="G:\资料\做项目\VueProjects\ZJ_PM\src\page\SystemSet.vue">基础设置</a>
+        </el-breadcrumb-item>
+        <el-breadcrumb-item>用户设置</el-breadcrumb-item>
+      </el-breadcrumb>
+      <div>
+        <el-h2 padding-left="20px;">用户设置</el-h2>
+        <el-input
+          type="text"
+          placeholder="请输入内容"
+          prefix-icon="el-icon-search"
+          v-model="input1"
+          style="width:200px;margin-left:20px;"
+        ></el-input>
+        <el-button type="primary" icon="el-icon-search">搜索</el-button>
+        <el-button type="primary" icon="el-icon-edit"></el-button>
+        <el-button type="primary" icon="el-icon-share"></el-button>
+        <el-button type="primary" icon="el-icon-delete"></el-button>
+        <el-button type="primary">
+          上传
+          <i class="el-icon-upload el-icon--right"></i>
+        </el-button>
+      </div>
+    </el-header>
 
-  <script>
-    export default {
-      data() {
-        return {
-          tableData: [{
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄'
-          }, {
-            date: '2016-05-04',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1517 弄'
-          }, {
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄'
-          }, {
-            date: '2016-05-03',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1516 弄'
-          }, {
-            date: '2016-05-03',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1516 弄'
-          }, {
-            date: '2016-05-03',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1516 弄'
-          }, {
-            date: '2016-05-03',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1516 弄'
-          }, {
-            date: '2016-05-03',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1516 弄'
-          },{
-            date: '2016-05-03',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1516 弄'
-          }
-          ]
-        }
-      }
-    }
-  </script>
+    <el-main>
+      <el-table :data="tableData" border height="550px">
+        <el-table-column prop="name" label="姓名" width="150"></el-table-column>
+        <el-table-column prop="userName" label="用户名" width="150"></el-table-column>
+        <el-table-column prop="password" label="密码" width="150">
+          <div>
+            <el-input placeholder="123" type="text" v-model="text"></el-input>
+          </div>
+        </el-table-column>
+        <el-table-column prop="department" label="部门" width="150"></el-table-column>
+        <el-table-column prop="address" label="地址"></el-table-column>
+      </el-table>
+    </el-main>
+   
+  </el-container>
+</template>
+
+<script>
+export default {
+  data() {
+    const item = {
+      userName: "wxh",
+      password: "123",
+      department: "工程部",
+      name: "王小虎",
+      address: "上海市普陀区金沙江路 1518 弄"
+    };
+    return {
+      tableData: Array(5).fill(item),
+      input: "",
+      input1: "",
+      textarea: "",
+      text: ""
+    };
+  }
+};
+</script>
 
 
-  <style scoped >
-  
-.el-container{
-    background-color: red;
+<style scoped >
+.el-container {
+  padding: 0;
+  margin: 0;
+}
+.el-header {
+  width: 100%;
+  height: 80px !important;
+  background-color: rgb(255, 255, 255);
+  color: #333;
+  text-align: left;
+  margin-right: 150px;
+  line-height: 60px;
+  font-size: 20px;
+  z-index: 9999;
+  top: 0px;
+  position: absolute;
+}
+.el-breadcrumb {
+  margin-top: 10px;
+}
+.el-main {
+  padding: 0px;
+  width: 100%;
+  padding-bottom: 160px;
+
+  padding-top: 80px;
+  height: 100%;
+}
+
+.el-table {
+  padding: 0px;
+  height: 550px;
+}
+.el-footer {
+  padding: 0px;
+  height: 160px;
+  width: 100%;
+
+  bottom: 0px;
+  position: fixed;
+  background-color: rgb(255, 255, 255);
+  color: #333;
+  text-align: left;
+  margin-right: 150px;
+
+  font-size: 20px;
+
+  position: fixed;
+  overflow: auto;
+}
+.el-tabs {
+  width: 100%;
+  padding: 0px;
+  bottom: 0px;
+  height: 210px;
+
+  position: fixed;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -9,11 +9,41 @@ import SystemSet from '@/page/SystemSet'
 import PersonInfo from '@/page/SystemSetPages/PersonInfo'
 import BaseSet from '@/page/SystemSetPages/BaseSet'
 import TaskManager from '@/page/TaskManager'
+import Login from '@/page/Login'
+import BasicSet from '@/page/BasicSet'
+import PasswordSet from '@/page/SystemSetPages/PasswordSet'
+import TotalSet from '@/page/SystemSetPages/TotalSet'
+import PersonalSet from '@/page/SystemSetPages/PersonalSet'
+import Base from '@/page/SystemSetPages/Base'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [{
+    path: '/',
+    name: 'Login',
+    component: Login,
+    meta: {
+      keepAlive: true
+    }
+  },
+  {
+    path: '',
+    name: 'Login',
+    component: Login,
+    meta: {
+      keepAlive: true
+    }
+  },
+  {
+    path: '#',
+    name: 'Login',
+    component: Login,
+    meta: {
+      keepAlive: true
+    }
+  },
+  {
     path: '/',
     name: 'Main',
     component: Main,
@@ -74,6 +104,43 @@ export default new Router({
         meta: {
           keepAlive: true
         }
+      }, {
+        path: '/SystemSetPages/PasswordSet',
+        name: 'PasswordSet',
+        component: PasswordSet,
+        meta: {
+          keepAlive: true
+        }
+      }, {
+        path: '/SystemSetPages/TotalSet',
+        name: 'TotalSet',
+        component: TotalSet,
+        meta: {
+          keepAlive: true
+        }
+      },
+      {
+        path: '/Pages/BasicSet',
+        name: 'BasicSet',
+        component: BasicSet,
+        meta: {
+          keepAlive: true
+        }
+      }, {
+        path: '/SystemSetPages/PersonalSet',
+        name: 'PersonalSet',
+        component: PersonalSet,
+        meta: {
+          keepAlive: true
+        },
+        children: [{
+          path: '/SystemSetPages/Base',
+          name: 'Base',
+          component: Base,
+          meta: {
+            keepAlive: true
+          }
+        }]
       }],
       meta: {
         keepAlive: true

6e6a6fc97e174cc18552a5dee83bc914ca3829c1
Jay <1547059772@qq.com>
2019-09-27 16:04:52 +0800

    鄢敏杰-9/27-login更改

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Main from '@/page/Main'
-import ProjectReport from '@/page/ProjectReport'
+
 import ProjectManager from '@/page/ProjectManager'
 import ProjectBaseData from '@/page/ProjectBaseData'
 import KnowledgeBase from '@/page/KnowledgeBase'
@@ -67,15 +67,6 @@ export default new Router({
       path: '/TaskManager',
       name: 'TaskManager',
       component: TaskManager,
-      meta: {
-        keepAlive: true
-      }
-    },
-
-    {
-      path: '/ProjectReport',
-      name: 'ProjectReport',
-      component: ProjectReport,
       meta: {
         keepAlive: true
       }

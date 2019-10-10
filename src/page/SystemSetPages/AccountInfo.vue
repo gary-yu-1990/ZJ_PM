b20e88b58b908675ff293e91ae769508212b001c
Jay <1547059772@qq.com>
2019-10-10 21:16:31 +0800

    新建 查询 功能完善

--- src/page/SystemSetPages/AccountInfo.vue
+++ src/page/SystemSetPages/AccountInfo.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="pagePanel">
-
     <div class="filterPanel">
       <el-date-picker
         type="date"
@@ -27,28 +26,36 @@
         ></el-option>
       </el-select>
     </div>
-
     <div class="buttonPanel">
-      <el-button size="medium" @click="newOne" type="primary">新建</el-button>
-      <el-button size="medium" type="success" @click="searchBankList()">查询</el-button>
+      <el-button size="medium" @click="Add" type="primary">新建</el-button>
+      <el-button size="medium" @click="Delete" type="primary">批量删除</el-button>
+      <el-button size="medium" type="success" @click="Search">查询</el-button>
     </div>
-    
     <div class="contentPanel">
       <div class="content">
         <el-table
           ref="multipleTable"
-          :data="tableData"
+          :data="userListData"
           height="100%"
           tooltip-effect="dark"
           style="width: 100%;"
           @selection-change="handleSelectionChange"
         >
           <el-table-column type="selection" width="55"></el-table-column>
-          <el-table-column label="日期" width="120">
-            <template slot-scope="scope">{{ scope.row.date }}</template>
+          <el-table-column prop="UserID" label="用户编码" width="120"></el-table-column>
+          <el-table-column prop="UserName" label="用户名" width="150"></el-table-column>
+          <el-table-column prop="EmployeeCode" label="工号" width="150"></el-table-column>
+          <el-table-column prop="UserSex" label="性别" width="150"></el-table-column>
+            <el-table-column label="操作">
+            <template slot-scope="scope">
+              <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+              <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+            </template>
           </el-table-column>
-          <el-table-column prop="name" label="姓名" width="120"></el-table-column>
-          <el-table-column prop="address" label="地址" show-overflow-tooltip></el-table-column>
+
+          <!-- <el-table-column label="日期" width="120">
+            <template slot-scope="scope">{{ scope.row.date }}</template>
+          </el-table-column>-->
         </el-table>
       </div>
       <div class="tbar" style="margin:0 25%;">
@@ -61,116 +68,157 @@
         ></el-pagination>
       </div>
     </div>
+
+    <el-dialog title="新建项目" :visible.sync="isAddShow" :close-on-click-modal="false" width="50%">
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-row>
+          <el-col span="11">
+            <el-form-item label="用户编号">
+              <el-input v-model="form.UserID"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col span="11">
+            <el-form-item label="用户名称">
+              <el-input v-model="form.UserName"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col span="11">
+            <el-form-item label="工号">
+              <el-input v-model="form.EmployeeCode"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col span="11">
+            <el-form-item label="性别">
+              <el-input v-model="form.UserSex"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+
+        <el-form-item style="margin-left: 240px;">
+          <el-button type="primary" @click="submitForm()">保存</el-button>
+          <el-button @click="resetForm('ruleForm')">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
 <script>
+import { SearchUsers } from "@/api/UsersInfoApi";
+import { NewUsers } from "@/api/UsersInfoApi";
 export default {
   data() {
     return {
-      tableData: [
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-08",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-06",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-07",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-07",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-07",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-07",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-07",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-07",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-07",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-07",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-07",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-07",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-07",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        }
-      ],
-      multipleSelection: []
+      userListData: [],
+      multipleSelection: [],
+      isAddShow: false,
+      handType: "add",
+      form: {
+        UserID: "",
+        UserName: "",
+        EmployeeCode: "",
+        UserSex: "女"
+      }
     };
   },
 
   methods: {
-    toggleSelection(rows) {
-      if (rows) {
-        rows.forEach(row => {
-          this.$refs.multipleTable.toggleRowSelection(row);
+    SearchUsersMethod() {
+      SearchUsers()
+        .then(res => {
+          this.userListData = res.data;
+        })
+        .catch(err => {
+          this.$alert(`${err.msg}`, "提示", {
+            type: "warning"
+          });
         });
-      } else {
-        this.$refs.multipleTable.clearSelection();
-      }
     },
     handleSelectionChange(val) {
       this.multipleSelection = val;
+    },
+    Add() {
+      this.form = {};
+      this.handType = "add";
+      this.isAddShow = true;
+    },
+    Search() {
+      this.SearchUsersMethod();
+    },
+    Delete() {
+      DeleteLists(this.multipleSelection)
+        .then(res => {
+          this.SearchUsersMethod();
+        })
+        .catch(err => {
+          this.$alert(`${err.msg}`, "提示", {
+            type: "warning"
+          });
+        });
+    },
+    handleEdit(index, row) {
+      this.handType = "edit";
+      this.form = row;
+      this.isAddShow = true;
+    },
+    handleDelete(index, row) {
+      var deleteArry = [];
+      deleteArry.push(row);
+      DeleteLists(deleteArry)
+        .then(res => {
+          this.SearchUsersMethod();
+        })
+        .catch(err => {
+          this.$alert(`${err.msg}`, "提示", {
+            type: "warning"
+          });
+        });
+    },
+    submitForm() {
+      //判断是新增还是编辑更新
+      if (this.handType === "add") {
+        NewUsers(this.form)
+          .then(res => {
+            this.$alert(`保存成功`, "提示", {
+              type: "warning",
+              confirmButtonText: "好的"
+            });
+            this.isAddShow = false;
+            this.SearchUsersMethod();
+          })
+          .catch(err => {
+            this.$alert(`${err.msg}`, "提示", {
+              type: "warning",
+              confirmButtonText: "好的"
+            });
+          });
+      }
+      if (this.handType === "edit") {
+        UpdateUser(this.form)
+          .then(res => {
+            this.$alert(`更新成功`, "提示", {
+              type: "warning",
+              confirmButtonText: "好的"
+            });
+            this.isAddShow = false;
+            this.SearchUsersMethod();
+          })
+          .catch(err => {
+            this.$alert(`${err.msg}`, "提示", {
+              type: "warning",
+              confirmButtonText: "好的"
+            });
+          });
+      }
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
     }
   }
 };
@@ -182,6 +230,7 @@ export default {
   height: 100%;
   display: flex;
   flex-direction: column;
+  overflow: hidden;
   /* background-color: rgb(231, 160, 160); */
 }
 .filterPanel {
@@ -202,9 +251,7 @@ export default {
 }
 
 .contentPanel .content {
- overflow: hidden;
+  overflow: hidden;
   flex: 1;
-}
-.contentPanel .tbar {
 }
 </style>
